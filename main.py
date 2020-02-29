0e75d1ae33cca7e304a1a8a5d89e11d6334cbfaf
Pavel Lepekhin <ue72785@sbb.ch>
2020-02-29 20:59:19 +0300

    get first recommendations

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-import scipy.io as sio
-import numpy as np
+import time
 
+import numpy as np
+import scipy as scipy
 from scipy.io import loadmat
+from scipy.optimize import minimize
 
 from ml.checkCostFunction import checkCostFunction
-from ml.cost import cofiCostFunc
+from ml.cost import cofiCostFuncCost, cofiCostFuncGrad
 from ml.loadMovieList import loadMovieList
+from ml.normalizeRatings import normalizeRatings
 
 print('Loading movie ratings dataset.\n')
 mat_contents = loadmat('ml/resources/ex8_movies.mat')
@@ -43,7 +46,7 @@ R = R[0:num_movies, 0:num_users]
 # shape matrix to vector for quick calculations
 params = np.concatenate((X.flatten(), Theta.flatten()))
 # Evaluate cost function
-J, grad = cofiCostFunc(params, Y, R, num_users, num_movies, num_features, 0)
+J = cofiCostFuncCost(params, Y, R, num_users, num_movies, num_features, 0)
 
 print('[Cost at loaded parameters: ', J, '(this value should be about 22.22)]')
 
@@ -64,7 +67,7 @@ checkCostFunction()
 #
 
 #  Evaluate cost function
-J, grad = cofiCostFunc(params, Y, R, num_users, num_movies, num_features, 1.5)
+J = cofiCostFuncCost(params, Y, R, num_users, num_movies, num_features, 1.5)
 
 print('Cost at loaded parameters (lambda = 1.5): ', J, '\n(this value should be about 31.34)\n')
 
@@ -91,24 +94,119 @@ my_ratings = np.zeros(len(movieList))
 
 # Check the file movie_idx.txt for id of each movie in our dataset
 # For example, Toy Story (1995) has ID 1, so to rate it "4", you can set
-my_ratings[1] = 4
+my_ratings[1-1] = 4
 
 # Or suppose did not enjoy Silence of the Lambs (1991), you can set
-my_ratings[98] = 2
+my_ratings[98-1] = 2
 
 # We have selected a few movies we liked / did not like and the ratings we
 # gave are as follows:
-my_ratings[7] = 3
-my_ratings[12] = 5
-my_ratings[54] = 4
-my_ratings[64] = 5
-my_ratings[66] = 3
-my_ratings[69] = 5
-my_ratings[183] = 4
-my_ratings[226] = 5
-my_ratings[355] = 5
+my_ratings[7-1] = 3
+my_ratings[12-1] = 5
+my_ratings[54-1] = 4
+my_ratings[64-1] = 5
+my_ratings[66-1] = 3
+my_ratings[69-1] = 5
+my_ratings[183-1] = 4
+my_ratings[226-1] = 5
+my_ratings[355-1] = 5
 
-print('\n\nNew user ratings:\n')
-for i in range(1, len(my_ratings)):
+# print('\n\nNew user ratings:\n')
+# for i in range(len(my_ratings)):
+#     if my_ratings[i] != 0:
+#         print('Rated ', my_ratings[i], ' for ', movieList[i+1])
+
+## ================== Part 7: Learning Movie Ratings ====================
+#  Now, you will train the collaborative filtering model on a movie rating 
+#  dataset of 1682 movies and 943 users
+#
+
+print('\nTraining collaborative filtering...\n')
+
+#  Load data
+mat_contents = loadmat('ml/resources/ex8_movies.mat')
+(Y, R) = (mat_contents.get("Y"), mat_contents.get("R"))
+
+#  Y is a 1682x943 matrix, containing ratings (1-5) of 1682 movies by 
+#  943 users
+#
+#  R is a 1682x943 matrix, where R(i,j) = 1 if and only if user j gave a
+#  rating to movie i
+
+#  Add our own ratings to the data matrix
+Y = np.column_stack((my_ratings, Y))
+R = np.column_stack((my_ratings != 0, R))
+
+# Test example from exercies
+num_features = 10 #set 10
+#Y = np.array([[5, 5, 1, 1], [5, 0, 0, 1], [0, 4, 1, 0], [1, 1, 5, 4], [1, 1, 5, 0]])
+#R = np.array([[1, 1, 1, 1], [1, 0, 0, 1], [0, 1, 1, 0], [1, 1, 1, 1], [1, 1, 1, 0]])
+
+# #  Normalize Ratings
+(Ynorm, Ymean) = normalizeRatings(Y, R)
+# print('Ymean=', Ymean)
+#
+#  Useful Values
+num_users = Y.shape[1]
+num_movies = Y.shape[0]
+
+#
+# Set Initial Parameters (Theta, X)
+X = np.random.rand(num_movies, num_features)
+Theta = np.random.rand(num_users, num_features)
+#
+initial_parameters = np.concatenate((X.flatten(), Theta.flatten()))
+#
+# # Set Regularization
+lambda_reg = 10
+
+start_time = time.time()
+res1 = scipy.optimize.fmin_cg(cofiCostFuncCost, initial_parameters, fprime=cofiCostFuncGrad, args=(Ynorm, R, num_users, num_movies, num_features, lambda_reg))
+print("--- %s seconds ---" % (time.time() - start_time))
+
+# print ('res1 = ', res1)
+# theta = minimize(cofiCostFuncSingle, initial_parameters,
+#                 args=(Ynorm, R, num_users, num_movies, num_features, lambda_reg), method='Nelder-Mead')
+# theta = theta.x
+#
+# Unfold the returned theta back into X and Theta
+X = np.reshape(res1[0:num_movies * num_features], (num_movies, num_features))
+Theta = np.reshape(res1[num_movies * num_features:], (num_users, num_features))
+
+print('Recommender system learning completed.\n')
+
+print('\nProgram paused. Press enter to continue.\n')
+
+## ================== Part 8: Recommendation for you ====================
+#  After training the model, you can now make recommendations by computing
+#  the predictions matrix.
+#
+
+p = np.dot(X, Theta.T)
+userId = 0
+my_predictions = p[:, userId] + Ymean[:, 0]
+
+movieList = loadMovieList()
+print('\nTop recommendations for you:\n')
+r = sorted(enumerate(my_predictions),key=lambda x: x[1], reverse=True)
+count = 25
+for item in r:
+        print('Predicting rating %.1f for movie %s' % (item[1], movieList[item[0] + 1]))
+        count = count - 1
+        if count == 0:
+            break
+
+
+print('\n\nOriginal ratings provided:\n')
+for i in range(len(my_ratings)):
     if my_ratings[i] != 0:
-        print('Rated ', my_ratings[i], ' for ', movieList[i])
+        print('Rated %.1f (pred. %.1f) for %s' % (my_ratings[i], my_predictions[i], movieList[i+1]))
+
+print('\nCompare for 1 user:\n')
+userId = 1
+my_predictions = p[:, userId] + Ymean[:, 0]
+for i in range(num_movies):
+    if Y[i][userId] != 0:
+        print('Rated %.1f (pred. %.1f) for %s' % (Y[i][userId], my_predictions[i], movieList[i + 1]))
+
+print('Success!')
--- ml/cost.py
+++ ml/cost.py
@@ -1,5 +1,36 @@
 import numpy as np
 
+def cofiCostFuncCost(params, Y, R, num_users, num_movies, num_features, lambda_reg):
+    # Unfold the X and Theta matrices from params
+    X = params[0:num_movies * num_features].reshape(num_movies, num_features)
+    Theta = params[num_movies * num_features:].reshape(num_users, num_features)
+
+    # calculate difference between actual and expected rating of film
+    errMatrix = np.dot(X, Theta.T) * R - Y
+    J = (errMatrix * errMatrix).sum() / 2
+
+    # add regularization summands
+    theta_reg = lambda_reg / 2 * np.sum(Theta * Theta)
+    x_reg = lambda_reg / 2 * np.sum(X * X)
+    J = J + theta_reg + x_reg
+    return J
+
+def cofiCostFuncGrad(params, Y, R, num_users, num_movies, num_features, lambda_reg):
+    # Unfold the X and Theta matrices from params
+    X = params[0:num_movies * num_features].reshape(num_movies, num_features)
+    Theta = params[num_movies * num_features:].reshape(num_users, num_features)
+
+    # calculate difference between actual and expected rating of film
+    errMatrix = np.dot(X, Theta.T) * R - Y
+
+    # calculate gradients
+    X_grad = np.dot(errMatrix, Theta) + lambda_reg * X
+    Theta_grad = np.dot(errMatrix.T, X) + lambda_reg * Theta
+
+    grad = np.concatenate((X_grad.flatten(), Theta_grad.flatten()))
+
+    return grad
+
 def cofiCostFunc(params, Y, R, num_users, num_movies, num_features, lambda_reg):
     # COFICOSTFUNC Collaborative filtering cost function
     # [J, grad] = COFICOSTFUNC(params, Y, R, num_users, num_movies, num_features, lambda )
@@ -26,5 +57,6 @@ def cofiCostFunc(params, Y, R, num_users, num_movies, num_features, lambda_reg):
     Theta_grad = np.dot(errMatrix.T, X) + lambda_reg*Theta
 
     grad = np.concatenate((X_grad.flatten(), Theta_grad.flatten()))
+
     return J, grad
 
