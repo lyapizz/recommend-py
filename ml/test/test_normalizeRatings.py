3c81b05d35e79700492222773a2f019ae9a9dff9
Pavel Lepekhin <ue72785@sbb.ch>
2020-03-01 14:05:13 +0300

    unit for normalizeRatings

---
+++ ml/test/test_normalizeRatings.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+
+import numpy as np
+from ml.normalizeRatings import normalizeRatings
+
+class Test(TestCase):
+
+    def test_NormalizeRatings(self):
+        # Evaluate cost function
+        Y = np.array([[5, 5, 0, 0], [5, 0, 0, 0], [0, 4, 0, 0], [0, 0, 5, 4], [0, 0, 5, 0]])
+        R = np.array([[1, 1, 1, 1], [1, 0, 0, 1], [0, 1, 1, 0], [1, 1, 1, 1], [1, 1, 1, 0]])
+        (Ynorm, Ymean) = normalizeRatings(Y, R)
+
+        expectedYmean = np.array([[2.5], [2.5], [2], [2.25], [1.6666666666666667]])
+        expectedYnorm = np.array([[2.5, 2.5, -2.5, -2.5], [2.5, 0, 0, -2.5], [0, 2, -2, 0], [-2.25, -2.25, 2.75, 1.75], [-1.6666666666666667, -1.6666666666666667, 3.333333333333333, 0]])
+        self.assertTrue((expectedYmean == Ymean).all())
+        self.assertTrue((expectedYnorm == Ynorm).all())
