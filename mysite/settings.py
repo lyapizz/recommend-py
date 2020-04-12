882db6044a61b28df35f3ea6f640b53d44f584b1
Pavel Lepekhin <ue72785@sbb.ch>
2020-04-12 17:56:08 +0300

    step5: allow hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'n)-$p4n_dh*#rjysa!&lob3rvpfdi+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['secret-fjord-62055.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
