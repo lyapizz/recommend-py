d23c1df498aea031468e82b5ffcf155a8467a7d5
lyapizz <lyapizz@gmail.com>
2020-04-12 14:38:45 +0300

    debug false

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'n)-$p4n_dh*#rjysa!&lob3rvpfdi+4-xwsm$d5c4c+8c#u8ok'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
 ALLOWED_HOSTS = []
 
