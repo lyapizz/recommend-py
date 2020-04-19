4c5a3aacf617050c41c4cbb7acd86050bc6e8083
Pavel Lepekhin <ue72785@sbb.ch>
2020-04-19 17:42:19 +0300

    unit for prepareMyPredictions

--- polls/test/actions/test_actions.py
+++ polls/test/actions/test_actions.py
@@ -13,14 +13,14 @@ class Test(TestCase):
         cls._num_features = 1
 
         cls._params = dict()
-        cls._params['X'] = np.array([[1], [1], [1], [1], [1]])
-        cls._params['Theta'] = np.array([[1], [1], [1], [1]])
+        cls._params['X'] = np.zeros((cls._num_movies, cls._num_features))
+        cls._params['Theta'] = np.zeros((cls._num_users, cls._num_features))
         cls._params['Ymean'] = np.array([[1], [2], [3], [4], [5]])
 
         User.objects.create(id=1, username='user1')
         User.objects.create(id=3, username='user2')
         User.objects.create(id=4, username='user3')
-        User.objects.create(id=5, username='user4')
+        User.objects.create(id=99, username='user4')
 
     def test_collection_to_matrix_dict(self):
         # test
@@ -30,7 +30,7 @@ class Test(TestCase):
         expected[1] = 0
         expected[3] = 1
         expected[4] = 2
-        expected[5] = 3
+        expected[99] = 3
         self.assertEqual(expected, result)
 
     def test_matrix_to_collectionDict(self):
@@ -41,17 +41,14 @@ class Test(TestCase):
         expected[0] = User.objects.get(id=1)
         expected[1] = User.objects.get(id=3)
         expected[2] = User.objects.get(id=4)
-        expected[3] = User.objects.get(id=5)
+        expected[3] = User.objects.get(id=99)
         self.assertEqual(expected, result)
 
     def test_prepareMyPredictions_LastUser(self):
         # prepare
-        user = User.objects.get(id=5)
+        user = User.objects.get(id=99)
         # test
         result = prepareMyPredictions(self._params, user)
         # expect
-        expected = dict()
-        expected[0] = User.objects.get(id=1)
-        expected[1] = User.objects.get(id=3)
-        expected[2] = User.objects.get(id=4)
-        self.assertEqual(expected, result)
+        expected = np.array([[1], [2], [3], [4], [5]])
+        self.assertEqual(expected.all(), result.all())
