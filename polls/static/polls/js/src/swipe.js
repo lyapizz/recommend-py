feefb98b1b4c9f7c8b031181316b6968c2c162ed
Pavel Lepekhin <ue72785@sbb.ch>
2020-05-04 16:32:02 +0300

    not so stict deliminations for swipe

--- polls/static/polls/js/src/swipe.js
+++ polls/static/polls/js/src/swipe.js
@@ -5,10 +5,10 @@ var myElement = document.getElementById('detail-container');
 var mc = new Hammer(myElement);
 // listen to events...
 mc.on("panleft panright tap press", function (ev) {
-    if (ev.type === "panleft" && ev.deltaX < -100 && Math.abs(ev.deltaY) < 25) {
+    if (ev.type === "panleft" && ev.deltaX < -50 && Math.abs(ev.deltaY) < 25) {
         window.location.replace(location + "next");
         mc.stop()
-    } else if (ev.type === "panright" && ev.deltaX > 100 && Math.abs(ev.deltaY) < 25) {
+    } else if (ev.type === "panright" && ev.deltaX > 50 && Math.abs(ev.deltaY) < 25) {
         window.location.replace(location + "previous");
         mc.stop()
     }
