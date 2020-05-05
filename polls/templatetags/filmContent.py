11815e06b4d0bf26bca7f7c6a164ae8d4c26ebc4
Pavel Lepekhin <ue72785@sbb.ch>
2020-05-05 16:12:42 +0300

    perf optimizations

--- polls/templatetags/filmContent.py
+++ polls/templatetags/filmContent.py
@@ -9,6 +9,19 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def filmContent(context, film):
     request = context.get('request')
-    return loader.get_template('polls/filmContent.html').render({
-        'film': film
-    }, request=request)
+    return loader.get_template('polls/filmContent.html').render({'film': film}, request=request)
+
+
+@register.simple_tag(takes_context=True)
+def filmContent(context, film):
+    request = context.get('request')
+    prevFilmId = None
+    nextFilmId = None
+    if context.get('film') == film:
+        if context.get('prevFilm'):
+            prevFilmId = context.get('prevFilm').id
+        if context.get('nextFilm'):
+            nextFilmId = context.get('nextFilm').id
+
+    return loader.get_template('polls/filmContent.html').render(
+        {'film': film, 'prevFilmId': prevFilmId, 'nextFilmId': nextFilmId, }, request=request)
