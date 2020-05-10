5c345b7e175213e1e208bc9f0ceea8c1bb57e490
Pavel Lepekhin <ue72785@sbb.ch>
2020-05-11 00:05:40 +0300

    own star_ratings urls

--- polls/urls_stars.py
+++ polls/urls_stars.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from star_ratings import app_settings
 from .views import Rate
 
-app_name = 'star'
+app_name = 'polls'
 
 urlpatterns = [
     url(r'(?P<content_type_id>\d+)/(?P<object_id>' + app_settings.STAR_RATINGS_OBJECT_ID_PATTERN + ')/', Rate.as_view(),
