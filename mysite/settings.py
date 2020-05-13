697d7769b05c7375eeb10f3b211a395c530a13df
Pavel Lepekhin <ue72785@sbb.ch>
2020-05-13 22:52:47 +0300

    new header to measure response time

--- mysite/settings.py
+++ mysite/settings.py
@@ -54,16 +54,15 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'polls.middleware.StatsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'polls.middleware.SocialAuthExceptionMiddleware',
-    "compression_middleware.middleware.CompressionMiddleware",
+    'compression_middleware.middleware.CompressionMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
--- polls/middleware.py
+++ polls/middleware.py
@@ -1,4 +1,7 @@
+import time
+
 from django.shortcuts import render
+from django.utils.deprecation import MiddlewareMixin
 from social_core.exceptions import AuthCanceled
 from social_django.middleware import SocialAuthExceptionMiddleware
 
@@ -10,3 +13,17 @@ class SocialAuthExceptionMiddleware(SocialAuthExceptionMiddleware):
             return render(request, template_name='polls/home.html')
         else:
             raise exception
+
+
+class StatsMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        "Start time at request coming in"
+        request.start_time = time.time()
+
+    def process_response(self, request, response):
+        "End of request, take time"
+        total = time.time() - request.start_time
+
+        # Add the header.
+        response["X-total-time"] = int(total * 1000)
+        return response
